c1323ceddd3e713fd9c5c2a95fabeaac7fcb07b6
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-05-13 18:43:52 -0300

    feat(adt): add arrangement list

---
+++ abstract-data-types/arrangement-list.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <string.h>
+ 
+using namespace std;
+
+typedef int ItemKey;
+
+class Item {
+	public:
+		Item();
+
+		Item(ItemKey itemKey) {
+			key = itemKey;
+		};
+
+		void setKey(ItemKey newKey) {
+			key = newKey;
+		};
+
+		ItemKey getKey() {
+			return key;
+		};
+
+	private:
+		ItemKey key;
+};
+
+class List {
+	public:
+		List() {
+			size = 0;
+		};
+
+		int getSize() {
+			return size;
+		};
+
+		bool isEmpty() {
+			return size == 0;
+		};
+
+		virtual Item getItem(int pos) = 0;
+		virtual void setItem(Item item, int pos) = 0;
+		virtual void insertStart(Item item) = 0;
+		virtual void insertEnd(Item item) = 0;
+		virtual void insert(Item item, int pos) = 0;
+		virtual Item removeStart() = 0;
+		virtual Item removeEnd() = 0;
+		virtual Item remove(int pos) = 0;
+		virtual Item search(ItemKey c) = 0;
+		virtual void clear() = 0;
+
+	protected:
+		int size;
+};
+
+class ArrangementList: public List {
+	public:
+		ArrangementList(): List() {};
+
+		Item getItem(int index) {
+			return itens[index];
+		};
+
+		void setItem(Item item, int index) {
+			itens[index] = item;
+		};
+
+		void insertStart(Item item) {
+			itens[0] = item;
+		};
+
+		void insertEnd(Item item) {
+			itens[size - 1] = item;
+		};
+
+		void insert(Item item, int index) {
+			itens[index] = item;
+		};
+
+		Item removeStart() {
+			return itens[0];
+		};
+
+		Item removeEnd() {
+			return itens[size - 1];
+		};
+
+		Item remove(int index) {
+			Item item;
+
+			itens[index] = item;
+		};
+
+		Item search(ItemKey selectedKey) {
+			for (int index = 0; index < size; index++) {
+				Item item = itens[index];
+
+				ItemKey itemKey = item.getKey();
+
+				if (itemKey == selectedKey) {
+					return item;
+				}
+			}
+		};
+
+		void clear();
+
+	private:
+		static const int MAXTAM = 100;
+		Item itens[MAXTAM];
+};
+ 
+int main() {
+	ArrangementList *arrangementList;
+	arrangementList = new ArrangementList();
+	
+	for (int i = 0; i < 10; i++) {
+		Item item;
+		item.setKey(i);
+		
+		arrangementList->insert(item, i);
+	}
+
+	for (int i = 0; i < arrangementList->getSize(); i++) {
+		Item item = arrangementList->getItem(i);
+		
+		cout << "retrieved item value: " << item.getKey() << endl;
+	}
+
+	return 0;
+};
